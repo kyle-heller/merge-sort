83d949249b6ff3fb5b060ae8cb57d43d7c77da14
kyle.heller <kyle.heller@gmail.com>
2022-12-16 16:30:44 -0500

    Added implentation of merge sort

---
+++ merge-sort.rb
@@ -0,0 +1,29 @@
+
+def merge_sort(array)
+  if array.length <= 1
+    return array
+  else
+    left = merge_sort(array[0...array.length/2])
+    right = merge_sort(array[array.length/2..-1])
+    return merge(left, right)
+  end
+end
+
+def merge(left, right, merged = [])
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      merged << left.shift
+    elsif left.first >= right.first
+      merged << right.shift
+    end
+  end
+  merged + left + right
+end
+
+
+arr = []
+50.times do
+  arr << rand(100)
+end
+
+print merge_sort(arr)
